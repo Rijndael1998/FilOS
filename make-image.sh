453843e84f0b65039b0d154eec49228991ec1709
Lukasz Baldyga <rijndael@baldy.ga>
2024-01-12 20:12:04 +0000

    Add user "v" with home directory and update script to list home directories.
    The commit adds the option "-m" to the "useradd" command in order to create a new user with a home directory. Additionally, the script is updated to include a command that lists the contents of the "/home/" directory after creating the user.

--- make-image.sh
+++ make-image.sh
@@ -7,6 +7,10 @@ pwd
 echo "[i] fs:"
 ls -la
 
-useradd v
+useradd -m v
+
+echo "[i] homes:"
+ls -la /home/
+
 cp -R /workspace/cdd/ /home/v/
 su -c "cd /home/v/cdd/ && build-simple-cdd --profiles test" v
